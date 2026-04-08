bfb3e67a3eef61cdcd07b66d6b9c9ffbf93e7dc7
jade <20237711@s.ubaguio.edu>
2026-04-08 15:12:55 +0800

    feat: add editingId state, startEdit() and cancelEdit(), add update flow in onSubmit(), refresh list after delete

--- src/app/pokemon-form/pokemon-form.component.ts
+++ src/app/pokemon-form/pokemon-form.component.ts
@@ -17,19 +17,49 @@ export class PokemonFormComponent implements OnInit {
     nature: ['', Validators.required]
   })
   pokemonService = inject(PokemonService);
+  editingId = signal<string | null>(null);
+
   ngOnInit(){
     this.pokemonService.fetchPokemon();
   }
+
+  deletePokemon(id: string){
+    if (confirm("Are you sure you want to release this pokemon?")){
+      this.pokemonService.deletePokemon(id);
+    }
+    this.pokemonService.fetchPokemon();
+  }
+  
+  startEdit(pokemon: any){
+    this.editingId.set(pokemon._id);
+    this.pokemonForm.patchValue(pokemon);
+  }
+
+  cancelEdit(){
+    this.editingId.set(null);
+    this.pokemonForm.reset();
+  }
+
   onSubmit(){
     if(this.pokemonForm.invalid) return;
-
     const data = this.pokemonForm.getRawValue();
-    this.pokemonService.savePokemon(data).subscribe({
+    const id = this.editingId();
+    if(id){
+      this.pokemonService.updatePokemon(id, data).subscribe({
+        next: () =>{
+          this.pokemonService.fetchPokemon();
+          this.cancelEdit();
+        },
+        error: (err) => console.error("Update Failed", err)
+      })
+    } else {
+      this.pokemonService.savePokemon(data).subscribe({
       next: () => {
         this.pokemonService.fetchPokemon();
         this.pokemonForm.reset();
       },
       error: (err) => console.error("Save Failed", err)
     });
+    }
   }
 }
